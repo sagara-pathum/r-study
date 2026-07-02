cabb7236e7a7158d22f70ce03c8d9191c888e57a
sagara-pathum <sagarapathum0618@gmail.com>
2026-07-02 21:15:25 +0530

    .

--- factors.R
+++ factors.R
@@ -16,3 +16,25 @@ str(df2)
 
 df2 <-as.data.frame(cbind(x2,y))
 str(df2)
+
+
+##converting a exiting variable as a factor
+
+x3 <- c(1:3)
+df3 <- cbind.data.frame(x3,y)
+
+df3
+str(df3)
+
+df3$x3 <- factor(df3$x3, levels = c(1,2,3))
+str(df3)
+
+
+###########################################
+
+x4 <- c(1:3)
+df4 <- cbind.data.frame(x4,y)
+df4$x4 <- factor(df4$x4,levels = c(1,2,3),labels = c("windows","macos","linux"))
+df4
+typeof(df4$x4)
+str(df4)
